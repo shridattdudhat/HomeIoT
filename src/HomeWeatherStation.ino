8b97ba0cb3ba3ab34b37d101f47db7252a511975
Shridatt Dudhat <shridattdudhat@outlook.com>
2017-12-25 22:54:18 +0530

    update

--- src/HomeWeatherStation.ino
+++ src/HomeWeatherStation.ino
@@ -9,10 +9,12 @@
 #include <Adafruit_GFX.h>
 
 #define BLYNK_PRINT Serial
+#define BLYNK_MAX_SENDBYTES 256
 
 #include "blynk.h"
 
 char auth[] = "55eff345a94e4b27afc68b11b99ca245";
+//long rssi = WiFi.RSSI();
 
 #define ONE_DAY_MILLIS (24 * 60 * 60 * 1000)
 unsigned long locTime;
@@ -20,6 +22,7 @@ unsigned long locTime;
 
 bool resetFlag = false;
 int cloudResetFunction(String Command);
+int cloudTweetFunction(String Command);
 
 Adafruit_Si7021 sensor = Adafruit_Si7021();
 #define OLED_RESET 4
@@ -27,58 +30,67 @@ Adafruit_SSD1306 display(OLED_RESET);
 
  void setup()
  {
-        Particle.function("reboot",cloudResetFunction);
-        sensor.begin();
-        Blynk.begin(auth);
-        display.begin(SSD1306_SWITCHCAPVCC, 0x3C); 
-   //Serial.begin(9600);
+    Particle.function("reboot",cloudResetFunction);
+    Particle.function("tweet",cloudTweetFunction);
+    sensor.begin();
+    Blynk.begin(auth);
+    display.begin(SSD1306_SWITCHCAPVCC, 0x3C); 
+    Time.zone(+5.50);
+    //Serial.begin(9600);
    
-        display.display();
-        delay(2000);
+    display.display();
+    delay(2000);
 
- // Clear the buffer.
-        display.clearDisplay();
-        display.setTextSize(1.8);
-        display.setCursor(0, 0);
-        display.setTextColor(WHITE);
+    //Clear the buffer.
+    display.clearDisplay();
+    display.setTextSize(1.8);
+    display.setCursor(0, 0);
+    display.setTextColor(WHITE);
  }
 
  void loop()
  {
-     
     locTime = Time.local();
-    
     Blynk.run();
-    
-   if (resetFlag){
-        System.reset();}
+    if (resetFlag)
+    {
+        System.reset();
+    }
         
-     float hum = sensor.readHumidity();
-     float temp = sensor.readTemperature();
+    float hum = sensor.readHumidity();
+    float temp = sensor.readTemperature();
      
-     Blynk.virtualWrite(V0, temp);
-     Blynk.virtualWrite(V1, hum);
+    Blynk.virtualWrite(V0, temp);
+    Blynk.virtualWrite(V1, hum);
      
-     display.setCursor(0, 0);
-     display.print(Time.format(locTime, TIME_FORMAT_ISO8601_FULL));
-    //  display.print();display.print();
+    display.setCursor(2, 4);
+    display.print(Time.format(Time.now(), "%Y-%m-%d | %I:%M%p"));
      
+    display.setCursor(5, 16);
+    display.print("Home Weather Station");
+    display.setCursor(5, 24);
+    display.print("--------------------");
+    
+    display.setCursor(0, 32);
+    display.print("Temperature");
+    display.setCursor(80, 32);
+    display.print("Humidity");
      
-     display.setCursor(0, 20);
-     display.print("Temp: ");
-     display.print(temp);display.print("*C");
-     display.setCursor(0, 30);
-     display.print("Hum: ");
-     display.print(hum);display.print("%");
+    display.setCursor(0, 42);
+    display.print(temp);display.print("*C");
+    display.setCursor(80, 42);
+    display.print(hum);display.print("%");
      
-     
-     display.display(); //you have to tell the display to...display
-     delay(2000);
-     display.clearDisplay();
+    display.setCursor(0,56);
+    display.print("WiFi : Connected !");
 
-     //Particle.publish("Temp",String(temp), PRIVATE);
-     //Particle.publish("Hum",String(hum), PRIVATE);
-     // Repeat after a 1 second pause
+    display.display(); //you have to tell the display to...display
+    delay(2000);
+    display.clearDisplay();
+
+    //Particle.publish("Temp",String(temp), PRIVATE);
+    //Particle.publish("Hum",String(hum), PRIVATE);
+    //Repeat after a 1 second pause
  }
 
 
@@ -87,4 +99,16 @@ int cloudResetFunction(String command)
   if (command == "reboot")
   resetFlag = true;
   return 1;
+}
+
+int cloudTweetFunction(String command)
+{   
+    float hum = sensor.readHumidity();
+    float temp = sensor.readTemperature();
+    if (command == "tweet")
+    Blynk.tweet("Tweet from HomeWeatherStation :: 'Hey, My #RaspberryPi can tweet now!' | #Blynk #Particle");
+    else if(command == "tweetTempHum")
+    Blynk.tweet(String("Tweet from HomeWeatherStation :: \n Temperature >>") + temp +"*C | Humidity >>" + hum +"% |\n #Ahmedabad #Weather #ElectronicsFun");
+    Particle.publish("Tweet",String("Tweet Done!"), PRIVATE);
+    return 1;
 }
